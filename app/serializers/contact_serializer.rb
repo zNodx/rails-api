7f919e2e464fa944cf8f337184e8e6372220bd59
zNodx <ffrmateus@gmail.com>
2022-03-29 11:15:34 -0300

    Phone number on json

---
+++ app/serializers/contact_serializer.rb
@@ -0,0 +1,11 @@
+class ContactSerializer < ActiveModel::Serializer
+  attributes :id, :name, :email, :birthdate
+
+  def attributes(*args)
+    h = super(*args)
+    # h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
+    h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
+    h
+  end
+
+end
